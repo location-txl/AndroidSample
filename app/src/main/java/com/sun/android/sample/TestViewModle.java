0065bb7ea68009be12e030f34cde203416a6e5f6
tianxiaolong <tianxiaolong@huami.com>
2019-11-29 14:51:07 +0800

    添加mvvm依赖

---
+++ app/src/main/java/com/sun/android/sample/TestViewModle.java
@@ -0,0 +1,15 @@
+package com.sun.android.sample;
+
+import androidx.lifecycle.ViewModel;
+
+/**
+ * @author tianxiaolong
+ * time：2019/11/29 14:45
+ * description：
+ */
+public class TestViewModle extends ViewModel {
+	@Override
+	protected void onCleared() {
+		super.onCleared();
+	}
+}
