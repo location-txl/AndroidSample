3a81226d4dc1aea1caeae39dc360c9378cf0594c
TLocation <tttx0307@163.com>
2020-02-05 15:24:04 +0800

    Revert "Feture lifecycle (#1)"
    This reverts commit 8b6c2d658b819d4f99ebf7f51cb7140de80ff93c.

--- app/src/main/java/com/sun/android/sample/MainActivity.java
+++ app/src/main/java/com/sun/android/sample/MainActivity.java
@@ -1,11 +1,6 @@
 package com.sun.android.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.lifecycle.ViewModelStore;
 
 import android.os.Bundle;
 
@@ -15,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		TestViewModle testViewModle = ViewModelProviders.of(this).get(TestViewModle.class);
 	}
 }
